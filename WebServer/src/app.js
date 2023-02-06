fb1eeb5bef5cfc89a30292fa96b6ad57f73830ee
Heitor Kunrath <60748906+HeitorKun@users.noreply.github.com>
2023-02-06 17:32:42 -0300

    404 and componentization

--- WebServer/src/app.js
+++ WebServer/src/app.js
@@ -1,17 +1,42 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
+//paths for express config
 const pathToPublicDir = path.join(__dirname, '../public')
+const pathToViews = path.join(__dirname, '../templates/views')
+const pathToPartials = path.join(__dirname, '../templates/partials')
 
+// set up habdle bars and view folder location
+app.set('view engine', 'hbs')
+app.set('views', pathToViews)
+hbs.registerPartials(pathToPartials)
+
+//setup static dir
 app.use(express.static(pathToPublicDir))
 
 app.get('', (req, res) => {
-    res.send('hello express')
+    res.render('index', 
+    {
+        title: "Weather",
+        creator: "Heitor K."
+    })
 })
 
-app.get('/help', (req, res) => {
-    res.send('help page')
+app.get('/about', (req, res) => {
+    res.render('about', 
+    {
+        title: "Weather",
+        creator: "Heitor K."
+    })
+})
+app.get('*', (req, res) => {
+    res.render('page404', 
+    {
+        title: "Weather",
+        creator: "Heitor K."
+    })
 })
 
 app.listen(3000, () => {
